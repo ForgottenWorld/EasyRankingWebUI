afe79702565a5e678ffb64922f1f3d2f67bd3133
Giacomo Zama <giacomozama@hotmail.it>
2020-06-09 19:40:10 +0200

    translucency on panels at edge, top bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import LeaderboardsGrid from './components/LeaderboardsGrid';
+import logo from './img/logo.png';
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-
-      </header>
+      <div className="app-header">
+        <img src={logo}></img>
+      </div>
       <div className="app-body">
         <LeaderboardsGrid></LeaderboardsGrid>
       </div>
--- src/components/LeaderboardsCell.js
+++ src/components/LeaderboardsCell.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import ReactVisibilitySensor from 'react-visibility-sensor';
 
-function LeaderboardsCell() {
-    return (
-        <div className="leaderboards-cell">
-            TEST
-            <div className="leaderboards-cell-list">
-                <div class="leaderboards-top3">
-                    <div>TIZIO<span class="lb-score">123456</span></div>
-                    <div>CAIO<span class="lb-score">123456</span></div>
-                    <div>SEMPRONIO<span class="lb-score">123456</span></div>
+class LeaderboardsCell extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            translucent: false
+        }
+    }
+
+    render() {
+        return (
+            <ReactVisibilitySensor onChange={(isVisible) => this.setState({translucent: isVisible})}>
+            { (isVisible) =>
+            <div className={`leaderboards-cell ${this.state.translucent ? "" : "translucent"}`}>
+                TEST
+                <div className="leaderboards-cell-list">
+                    <div class="leaderboards-top3">
+                        <div>TIZIO<span class="lb-score">123456</span></div>
+                        <div>CAIO<span class="lb-score">123456</span></div>
+                        <div>SEMPRONIO<span class="lb-score">123456</span></div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+            }
+            </ReactVisibilitySensor>
+        )
+    }
 }
 
 export default LeaderboardsCell;
