b888a4c55ed9aa6840b2d0b51a0e8b67f9efc515
Giacomo Zama <giacomozama@hotmail.it>
2020-06-09 15:29:23 +0200

    added scores, medals (to be changed), minecrafter font

--- src/components/LeaderboardsCell.js
+++ src/components/LeaderboardsCell.js
@@ -6,9 +6,9 @@ function LeaderboardsCell() {
             TEST
             <div className="leaderboards-cell-list">
                 <div class="leaderboards-top3">
-                    <div>TIZIO</div>
-                    <div>CAIO</div>
-                    <div>SEMPRONIO</div>
+                    <div>TIZIO<span class="lb-score">123456</span></div>
+                    <div>CAIO<span class="lb-score">123456</span></div>
+                    <div>SEMPRONIO<span class="lb-score">123456</span></div>
                 </div>
             </div>
         </div>
